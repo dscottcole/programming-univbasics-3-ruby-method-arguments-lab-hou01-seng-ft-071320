a3a22606c88d705272474dbc611a8497bf50c926
Daniel Cole <danielscole1@gmail.com>
2020-06-29 21:29:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,11 @@ end
 
 introduction_with_language("Tom","Ruby")
 
+def introduction_with_language_optional(name,language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Tom")
 
 
 
